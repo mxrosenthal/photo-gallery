34eff3c2dc9b092d058cb9e89d8c3b5ce133343c
Max Rosenthal <mxrosenthal@yahoo.com>
2020-04-30 10:52:58 -0700

    now able to save and load images from storage. Data persists if app is closed.

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -11,9 +11,13 @@ import {
   FilesystemDirectory,
 } from '@capacitor/core';
 
+//define constant to act as the key for the storage
+const PHOTO_STORAGE = 'photos';
+
 export function usePhotoGallery() {
   const { getPhoto } = useCamera();
   const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+  const { get, set } = useStorage(); //access the get and set methods from the useStorage hook.
   const [photos, setPhotos] = useState<Photo[]>([]);
 
   const savePicture = async (
@@ -34,6 +38,25 @@ export function usePhotoGallery() {
     };
   };
 
+  useEffect(() => {
+    const loadSaved = async () => {
+      const photoString = await get('photos');
+      const photos = (photoString ? JSON.parse(photoString) : []) as Photo[];
+
+      for (let photo of photos) {
+        const file = await readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data,
+        });
+        photo.base64 = `data:image/jpeg;base64,${file.data}`;
+      }
+      setPhotos(photos);
+    };
+    loadSaved(); // we have to call the async function from within the hook as the hook callback can't be asynchronous itself.
+  }, [get, readFile]);
+  // [get, readFile] is a dependancy array. because we've passed in a dependency array, the effect will only run if the dependency gets updated rather than each time the component renders.
+  // since get and readFile will never change, the data will only be called once.
+
   const takePhoto = async () => {
     const cameraPhoto = await getPhoto({
       resultType: CameraResultType.Uri,
@@ -45,6 +68,20 @@ export function usePhotoGallery() {
     const newPhotos = [savedFileImage, ...photos];
 
     setPhotos(newPhotos);
+
+    //adding set call to save the photos array. now data persists if the app is closed.
+    set(
+      PHOTO_STORAGE,
+      JSON.stringify(
+        newPhotos.map((p) => {
+          //don't save the base64 representation of the photo data,
+          //since it's already saved on the filesystem
+          const photoCopy = { ...p };
+          delete photoCopy.base64;
+          return photoCopy;
+        })
+      )
+    );
   };
 
   return {
